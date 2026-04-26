44a1a78b79a64f4e9c1e3f9c59d32f5b8d0a4988
sanchess2803 <88556220+mateuscostasanches@users.noreply.github.com>
2026-04-26 10:38:57 -0300

    Create G.cpp

---
+++ 2025_ProvaLocal/G.cpp
@@ -0,0 +1,17 @@
+// Problema G
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Leitura inicial
+	string entrada;
+	cin >> entrada;
+	
+	// Logica algoritmo
+	cout << entrada[0] << endl;
+		
+	return 0;
+}
